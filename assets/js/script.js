3f0ed28ebf69834530747f2aa94a568fea5ba902
rnc-personal <richard.challenger@protonmail.com>
2022-10-29 16:53:38 +0000

    fixed bug with random number generator

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,20 +7,27 @@ document.addEventListener('DOMContentLoaded', function() {
                 alert('clicked!')
             } else {
                 let gameType = this.getAttribute('data-type')
-                alert(`You Clicked ${gameType}`)
+                runGame(gameType)
             }
         })
     }
+    runGame('addition')
 })
 
 /**
  * This is the main game function
  */
-function runGame() {
+function runGame(gameType) {
 
-    letnum1 = Math.floor(Math.random()*25) + 1 
-    letnum2 = Math.floor(Math.random()*25) + 1 
+    let num1 = Math.floor(Math.random() * 25) + 1 
+    let num2 = Math.floor(Math.random() * 25) + 1 
     
+    if (gameType === 'addition') {
+        displayAdditionQuestion(num1, num2);
+    } else {
+        alert(`No game selected: ${gametype}`)
+        throw `No game selected: ${gametype}. Ending now!`
+    }
 }
 
 
@@ -40,9 +47,13 @@ function incrementWrongAnswer() {
     
 }
 
-function displayAdditionQuestion() {
+function displayAdditionQuestion(operand1, operand2) {
+    document.getElementById('operand1').textContent = operand1
+    document.getElementById('operand2').textContent = operand2
 
+    document.getElementById('operator').textContent = "+"
 }
+
 function  displaySubtractQuestion() {
     
 }
