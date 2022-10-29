f204ed66970bc327c006fbaeddf5fc5502778572
rnc-personal <richard.challenger@protonmail.com>
2022-10-29 16:00:56 +0000

    main game function defined

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,28 @@
+document.addEventListener('DOMContentLoaded', function() {
+    let buttons = document.getElementsByTagName('button')
+
+    for (let button of buttons) {
+        button.addEventListener('click', function(){
+            if (this.getAttribute('data-type') === 'submit') {
+                alert('clicked!')
+            } else {
+                let gameType = this.getAttribute('data-type')
+                alert(`You Clicked ${gameType}`)
+            }
+        })
+    }
+})
+
+/**
+ * This is the main game function
+ */
 function runGame() {
+
+    letnum1 = Math.floor(Math.random()*25) + 1 
+    letnum2 = Math.floor(Math.random()*25) + 1 
     
 }
+
 
 function checkAnswer() {
 
